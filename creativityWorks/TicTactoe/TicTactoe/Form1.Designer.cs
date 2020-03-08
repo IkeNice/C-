2772e5c063478f0534d43a3c07891598f6c66a9c
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-03-08 18:47:12 +0300

    TicTacToe

---
+++ creativityWorks/TicTactoe/TicTactoe/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace TicTactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 350);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button10.Location = new System.Drawing.Point(262, 372);
+            this.button10.Margin = new System.Windows.Forms.Padding(2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(61, 28);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Заново";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(334, 411);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ creativityWorks/TicTactoe/TicTactoe/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTactoe
+{
+    public partial class Form1 : Form
+    {
+        //private int x = 12, y = 12;
+        private Button[,] buttons = new Button[3, 3];
+        private int player;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Height = 450;
+            this.Width = 350;
+            player = 1;
+            label1.Text = "Текущий ход: Игрок 1";
+            for (int i = 0; i < buttons.Length / 3; i++)
+            {
+                for (int j = 0; j < buttons.Length / 3; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Size = new Size(100, 100);
+                }
+            }
+            setButtons();
+
+        }
+
+        private void setButtons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Location = new Point(12 + 106 * j, 12 + 106 * i);
+                    buttons[i, j].Click += button1_Click;
+                    buttons[i, j].Font = new Font(new FontFamily("Microsoft Sans Serif"), 60);
+                    buttons[i, j].Text = "";
+                    this.Controls.Add(buttons[i, j]);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sender.GetType().GetProperty("Text").SetValue(sender, "x");
+            player = 0;
+   
+            label1.Text = "Текущий ход: Игрок 2";
+            bot();
+            sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+            checkWin();
+        }
+
+        private void bot()
+        {
+            Random rnd = new Random();
+            for (int k=0; k<100; ++k)
+            {
+               int i = rnd.Next(0, 3);
+               int j = rnd.Next(0, 3);
+                if (buttons[i,j].Text=="")
+                { 
+                    buttons[i, j].Text = "о";
+                    buttons[i, j].Enabled = false;
+                    break;
+                }
+                  
+            }
+            player = 1;
+            label1.Text = "Текущий ход: Игрок 1";
+
+        }
+        private void checkWin()
+        {
+
+            if (buttons[0, 0].Text == buttons[0, 1].Text && buttons[0, 1].Text == buttons[0, 2].Text)
+            {
+
+                if (buttons[0, 0].Text == "x")
+                {
+                    MessageBox.Show("Вы победили!");
+                    return;
+                }
+            }
+            if (buttons[1, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[1, 2].Text)
+            {
+                if (buttons[1, 0].Text == "x")
+                    MessageBox.Show("Вы победили!");
+                if (buttons[1, 0].Text == "о")
+                    MessageBox.Show("Вы проиграли!");
+
+            }
+            if (buttons[2, 0].Text == buttons[2, 1].Text && buttons[2, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[2, 0].Text == "x")
+                    MessageBox.Show("Вы победили!");
+                if (buttons[2, 0].Text == "о")
+                    MessageBox.Show("Вы проиграли!");
+            }
+            if (buttons[0, 0].Text == buttons[1, 0].Text && buttons[1, 0].Text == buttons[2, 0].Text)
+            {
+                if (buttons[0, 0].Text == "x")
+                    MessageBox.Show("Вы победили!");
+                if (buttons[0, 0].Text == "о")
+                    MessageBox.Show("Вы проиграли!");
+            }
+            if (buttons[0, 1].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 1].Text)
+            {
+                if (buttons[0, 1].Text == "x")
+                    MessageBox.Show("Вы победили!");
+                if (buttons[0, 1].Text == "о")
+                    MessageBox.Show("Вы проиграли!");
+            }
+            if (buttons[0, 2].Text == buttons[1, 2].Text && buttons[1, 2].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 2].Text == "x")
+                    MessageBox.Show("Вы победили!");
+                if (buttons[0, 2].Text == "о")
+                    MessageBox.Show("Вы проиграли!");
+            }
+            if (buttons[0, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 0].Text == "x")
+                    MessageBox.Show("Вы победили!");
+                if (buttons[0, 0].Text == "о")
+                    MessageBox.Show("Вы проиграли!");
+            }
+            if (buttons[2, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[0, 2].Text)
+            {
+                if (buttons[2, 0].Text == "x")
+                    MessageBox.Show("Вы победили!");
+                if (buttons[2, 0].Text == "о")
+                    MessageBox.Show("Вы проиграли!");
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Текущий ход: Игрок 1";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Text = "";
+                    buttons[i, j].Enabled = true;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
