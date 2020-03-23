aa73470b6a4209b528a43483ef4a6ce18b4537b1
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-03-23 17:23:08 +0300

    done 9

---
+++ labs/lab9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string[] Strings = new string[255];
+            Console.Write("Введите количество строк: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Strings[i] = Console.ReadLine();
+            }
+            Console.Write("Введите подстроку для поиска: ");
+            string str_find = Console.ReadLine();
+            for (int i = 0; i < n; i++)
+            {
+                if (Strings[i].IndexOf(str_find) != -1)
+                {
+                    Console.WriteLine(i + 1 + ": " + Strings[i]);
+                }
+            }
+
+        }
+    }
+}
